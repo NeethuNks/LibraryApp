b20876dfb59ae3846b69efff1769d3b45ff2c0c4
Neethu K <neethunks20@gmail.com>
2022-02-05 14:48:41 +0530

    new

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors');
+const bodyParser = require('body-parser'); //Part #1 Point 2
+
+
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homeroute'); //Part #1 Point 3
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+//const nav =require('./src/data/nav'); //Part #2 Point 6
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors()); //Part #2 Point 7
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("Server Ready on 5000"); //Part #1 Point 5
+});
