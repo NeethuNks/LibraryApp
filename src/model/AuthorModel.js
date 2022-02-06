e9fa38be8824e2e1beaf396dd1356306591a9d75
Neethu K <neethunks20@gmail.com>
2022-02-06 15:14:55 +0530

    new

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect('mongodb+srv://Arjun:Norka123@cluster0.s5tyz.mongodb.net/Library?retryWrites=true&w=majority');
+//mongoose.connect('mongodb://localhost:27017/Library');
 const Schema = mongoose.Schema;
 
 
